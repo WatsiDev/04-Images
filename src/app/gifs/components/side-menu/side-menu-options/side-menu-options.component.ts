02d5321edc7101b7e08d182e708b075ab332fe6c
Jair Vázquez <jairvazquez604@gmail.com>
2025-03-06 14:51:01 -0600

    Hola

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface menuOption{
+  icon: string;
+  label: string;
+  route: string;
+  subLabel: string;
+}
 
 @Component({
   selector: 'app-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html'
 })
 export class SideMenuOptionsComponent {
-
+  menuOptions: menuOption[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route: '/dashboard/trending'
+    },
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar Gifs',
+      route: '/dashboard/search'
+    },
+  ]
 }
---
+++ src/environments/environment.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  companyName: 'Gifst',
+  companyName2: 'App',
+  companySlogan: 'Maneja tus gifs',
+
+  giphyApiKey: '',
+  giphyURL: ''
+};
